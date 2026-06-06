eafd33add42ebcb9be39adfef873e79ad3d446cc
ssi-118 <syedasoha118@gmail.com>
2026-06-06 11:41:05 +0530

    add ip/op file

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ["https://vercel.app"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 
 // Routes
